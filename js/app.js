b53e5d1309c8c2362e7d08fb7c299e69ca295c8a
Gopika Bejoy <gopikabejoy123@gmail.com>
2018-07-15 22:50:28 +0100

    Finished project

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+function refreshPage() {
+  window.location.reload();
+}
 // Enemies our player must avoid
 class Enemy {
   // Variables applied to each of our instances go here,
@@ -7,20 +10,43 @@ class Enemy {
   // a helper we've provided to easily load images
   constructor(x, y, speed) {
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
   }
 
 
 
   // Update the enemy's position, required method for game
   // Parameter: dt, a time delta between ticks
-  update (dt) {
+  update(dt) {
+    if (this.x > 510) {
+      this.x = 0
+    } else {
+
+      this.x = this.x + ((this.speed) * dt);
+
+    }
+    if (((player.y === 209) && (player.x - 57.5 < enemyThree.x) && (enemyThree.x < player.x + 50.5)) ||
+      ((player.y === 123.5) && (player.x - 57.5 < enemyTwo.x) && (enemyTwo.x < player.x + 50.5)) ||
+      ((player.y === 38) && (player.x - 57.5 < enemyOne.x) && (enemyOne.x < player.x + 50.5))) {
+        // For each row the bug is on, if statements check if the bug assigned to that row is near the player.
+      player.x = 200;
+      player.y = 380;
+
+
+    }
+
+
+
+
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
   }
 
   // Draw the enemy on the screen, required method for game
-  render () {
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
 };
@@ -28,8 +54,10 @@ class Enemy {
 // This class requires an update(), render() and
 // a handleInput() method.
 class Player {
-  constructor(x,y) {
-    this.sprite = 'images/char-cat-girl.png';
+  constructor(x, y) {
+    this.sprite = 'images/char-horn-girl.png';
+    this.x = x;
+    this.y = y;
   }
 
   update() {
@@ -41,16 +69,73 @@ class Player {
 
   }
 
-  handleInput() {
+  handleInput(key) {
+
+
+    if ((key === 'left') && (this.x > -2)) {
+      this.x -= 101;
+      console.log(this.x, this.y)
+
+    } else if ((key === 'right') && (this.x < 402)) {
+      this.x += 101;
+      console.log(this.x, this.y)
+
+    } else if ((key === 'up') && (this.y > -27.5)) {
+      this.y -= 85.5
+      console.log(this.x, this.y)
+
+      if (this.y === -47.5) { // creates new page once player reaches water
+        const resultsPage = document.createElement('div');
+        resultsPage.classList.add('resultsPage');
+        const heading = document.createElement('h1');
+        const congratulations = document.createTextNode("Congratulation! You've won!");
+        const buttonText = document.createTextNode('Replay');
+        const button = document.createElement("BUTTON");
+        button.setAttribute("onClick", "refreshPage()");
+        button.appendChild(buttonText);
+
+        heading.appendChild(congratulations);
+
+        resultsPage.appendChild(heading);
+        resultsPage.appendChild(button)
+
+        const canvas = document.querySelector('canvas');
+        canvas.remove();
+        // const body = document.getElementsByTagName('body')[0];
+        document.getElementsByTagName('body')[0].appendChild(resultsPage)
+
+        // body.appendChild('resultsPage');
+
+
+        console.log("You've won!")
+      }
+
+    } else if ((key === 'down') && (this.y < 380)) {
+      this.y += 85.5
+      console.log(this.x, this.y)
+
+
+    }
 
   }
+
 }
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 const allEnemies = [];
+speedOne = Math.floor((Math.random() * 100) + 90);
+speedTwo = Math.floor((Math.random() * 100) + 60);
+speedThree = Math.floor((Math.random() * 100) + 70);
+const enemyOne = new Enemy(31, 58, speedOne);
+const enemyTwo = new Enemy(30, 143.5, speedTwo);
+const enemyThree = new Enemy(25, 229, speedThree);
+
+allEnemies.push(enemyOne);
+allEnemies.push(enemyTwo);
+allEnemies.push(enemyThree);
 // Place the player object in a variable called player
-const player = new Player(20,25);
+const player = new Player(200, 380);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
